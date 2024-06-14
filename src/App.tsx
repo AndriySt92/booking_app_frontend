b740b00c384fa07bacac9d7d57e250f6ec2e8e29
AndriySt92 <andriyua-1992@mail.ru>
2024-06-14 19:44:53 +0300

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,112 @@
+import { Navigate, Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import Layout from './layout/Layout'
+import {
+  AddHotel,
+  Booking,
+  Detail,
+  EditHotel,
+  Home,
+  MyBookings,
+  MyHotels,
+  Search,
+  SignIn,
+  SignUp,
+} from './pages'
+import { PrivateRoutes, PublicRoutes } from './routes'
+
 function App() {
-  return <h1 className="text-3xl font-bold bg-grey-500 underline">Hello world!</h1>
+  const isLoggedIn = true
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Home />
+          </Layout>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <Layout>
+            <Search />
+          </Layout>
+        }
+      />
+      <Route
+        path="/detail/:hotelId"
+        element={
+          <Layout>
+            <Detail />
+          </Layout>
+        }
+      />
+      <Route
+        path="/sign-up"
+        element={
+          <Layout>
+            <SignUp />
+          </Layout>
+        }
+      />
+      <Route
+        path="/sign-in"
+        element={
+          <Layout>
+            <SignIn />
+          </Layout>
+        }
+      />
+
+      {isLoggedIn && (
+        <>
+          <Route
+            path="/hotel/:hotelId/booking"
+            element={
+              <Layout>
+                <Booking />
+              </Layout>
+            }
+          />
+
+          <Route
+            path="/add-hotel"
+            element={
+              <Layout>
+                <AddHotel />
+              </Layout>
+            }
+          />
+          <Route
+            path="/edit-hotel/:hotelId"
+            element={
+              <Layout>
+                <EditHotel />
+              </Layout>
+            }
+          />
+          <Route
+            path="/my-hotels"
+            element={
+              <Layout>
+                <MyHotels />
+              </Layout>
+            }
+          />
+          <Route
+            path="/my-bookings"
+            element={
+              <Layout>
+                <MyBookings />
+              </Layout>
+            }
+          />
+        </>
+      )}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+export { default as Home } from './Home';
+export { default as AddHotel } from './AddHotel';
+export { default as Booking } from './Booking';
+export { default as Detail } from './Detail';
+export { default as EditHotel } from './EditHotel';
+export { default as MyBookings } from './MyBookings';
+export { default as MyHotels } from './MyHotels';
+export { default as Search } from './Search';
+export { default as SignIn } from './SignIn';
+export { default as SignUp } from './SignUp';
