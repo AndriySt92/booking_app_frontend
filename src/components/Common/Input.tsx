ad70927ea998105a339ddc71e272a0058536c1fe
AndriySt92 <andriyua-1992@mail.ru>
2024-12-03 12:54:42 +0200

    refactor: update Input to support displaying checkbox and radio input types

--- src/components/Common/Input.tsx
+++ src/components/Common/Input.tsx
@@ -1,64 +1,112 @@
 import cn from 'classnames'
-import type { UseFormRegister, RegisterOptions } from 'react-hook-form'
-import { Error } from '..'
+import { UseFormRegister, RegisterOptions } from 'react-hook-form'
+import { Error } from '../'
 
 interface Props {
   name: string
   label?: string
   type?: string
   placeholder?: string
-  error?: string
+  error?: string | undefined
   register: UseFormRegister<any>
   validation?: RegisterOptions
   inputClassNames?: string
   labelClassNames?: string
   wrapperClassNames?: string
-  option?: string
+  option?: string // For checkbox or radio
+  selected?: boolean // To handle selected state for custom radios
+  multiple?: boolean // For file input
+  accept?: string // For file input
+  min?: number
+  max?: number
+  readOnly?: boolean
+  disabled?: boolean
 }
 
 const Input = ({
   name,
-  placeholder,
-  label = '',
+  label,
   type = 'text',
-  error = '',
+  error,
   register,
   validation,
   option,
+  selected,
   inputClassNames,
   labelClassNames,
   wrapperClassNames,
+  multiple,
+  accept,
+  ...rest
 }: Props) => {
-  const isCheckbox = type === 'checkbox'
+  const isTypeCheckbox = type === 'checkbox'
+  const isTypeRadio = type === 'radio'
+  const isTypeFile = type === 'file'
+
+  const checkboxLabelClasses = 'flex gap-3 items-center space-x-2 order-2'
+  const radioLabelClasses = 'rounded-full px-4 py-2 font-semibold transition-colors shadow-sm'
+  const radioSelectedLabelClasses = selected ? 'bg-blue-300 text-white' : 'bg-gray-300 text-black'
 
   return (
-    <div className={cn(wrapperClassNames, isCheckbox && 'flex items-center gap-2 mb-1')}>
-      {label && (
-        <label
-          htmlFor={isCheckbox ? option : name}
-          className={cn(
-            labelClassNames,
-            isCheckbox
-              ? 'flex gap-3 items-center space-x-2 order-2 cursor-pointer'
-              : 'text-gray-700 text-md sm:text-lg font-bold flex-1',
-          )}>
-          {label}
-        </label>
+    <div
+      className={cn(
+        wrapperClassNames,
+        isTypeCheckbox && 'flex items-center gap-2 mb-1',
+        isTypeRadio && 'inline-block cursor-pointer',
+        isTypeFile && 'flex flex-col gap-2',
+      )}>
+      {/* Default Input */}
+      {!isTypeRadio && !isTypeCheckbox ? (
+        <>
+          {label && (
+            <label
+              htmlFor={name}
+              className={cn(labelClassNames, 'text-gray-700 text-md sm:text-lg font-bold mb-1')}>
+              {label}
+            </label>
+          )}
+          <input
+            id={name}
+            type={type}
+            className={cn(
+              inputClassNames,
+              error ? 'border-red-600 border-2 shadow-md focus:ring-0' : '',
+              'border rounded w-full py-2 px-3 text-md font-normal focus:outline-none focus:ring focus:ring-blue-200',
+            )}
+            {...register(name, validation)}
+            {...(isTypeFile && { multiple: multiple, accept: accept })}
+            {...rest}
+          />
+        </>
+      ) : (
+        // Checkbox & Radio Button
+        <>
+          {label && (
+            <label
+              htmlFor={option}
+              className={cn(
+                labelClassNames,
+                'text-md sm:text-lg flex items-center cursor-pointer',
+                isTypeCheckbox ? checkboxLabelClasses : radioLabelClasses,
+                isTypeRadio && radioSelectedLabelClasses,
+              )}>
+              {label}
+            </label>
+          )}
+          <input
+            id={option}
+            type={type}
+            className={cn(
+              inputClassNames,
+              isTypeCheckbox ? 'h-5 w-5 cursor-pointer order-1' : 'hidden',
+            )}
+            {...register(name, validation)}
+            value={option}
+            {...rest}
+          />
+        </>
       )}
-      <input
-        id={isCheckbox ? option : name}
-        type={type}
-        placeholder={placeholder}
-        className={cn(
-          inputClassNames,
-          isCheckbox
-            ? 'h-5 w-5 cursor-pointer order-1'
-            : 'border rounded w-full py-1 px-2 font-normal',
-        )}
-        {...register(name, validation)}
-        {...(isCheckbox && { value: option })}
-      />
-      {error && <Error message={error} />}
+      {error && <Error message={error} size="small" />}
     </div>
   )
 }
