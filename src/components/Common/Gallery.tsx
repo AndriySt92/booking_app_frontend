3128952c39afba7528a8636d11a59043533ac9d1
AndriySt92 <andriyua-1992@mail.ru>
2024-12-05 19:56:34 +0200

    feat: add Gallery component to showcase images with responsive layout and react-photo-view

--- src/components/Common/Gallery.tsx
+++ src/components/Common/Gallery.tsx
@@ -1,37 +1,57 @@
-import ImageGallery from 'react-image-gallery';
-import 'react-image-gallery/styles/css/image-gallery.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view'
 
-const images = [
-    {
-      original: 'https://source.unsplash.com/800x600/?hotel',
-      thumbnail: 'https://source.unsplash.com/150x100/?hotel'
-    },
-    {
-      original: 'https://source.unsplash.com/800x600/?luxuryhotel',
-      thumbnail: 'https://source.unsplash.com/150x100/?luxuryhotel'
-    },
-    {
-      original: 'https://source.unsplash.com/800x600/?resorthotel',
-      thumbnail: 'https://source.unsplash.com/150x100/?resorthotel'
-    },
-    {
-      original: 'https://source.unsplash.com/800x600/?boutiquehotel',
-      thumbnail: 'https://source.unsplash.com/150x100/?boutiquehotel'
-    },
-    {
-      original: 'https://source.unsplash.com/800x600/?modernhotel',
-      thumbnail: 'https://source.unsplash.com/150x100/?modernhotel'
-    },
-    {
-      original: 'https://source.unsplash.com/800x600/?historichotel',
-      thumbnail: 'https://source.unsplash.com/150x100/?historichotel'
-    }
-  ];
+interface Props {
+  images: string[]
+}
 
-const Gallery = () => {
+const Gallery = ({ images }: Props) => {
+  const numberRestImages = images.length - 6
   return (
-    <ImageGallery items={images} />
-  );
-};
+    <PhotoProvider>
+      <div className="grid gap-2 sm:grid-cols-12 sm:grid-rows-5 sm:gap-4 grid-cols-10 grid-rows-5">
+        {images.map((image, index) => (
+          <div
+            key={image}
+            className={`group 
+            ${index === 0 ? 'sm:col-span-8 sm:row-span-4 col-span-10 row-span-4' : ''} 
+            ${
+              index === 1 ? 'sm:col-span-4 sm:row-span-2 sm:col-start-9 col-span-2 row-start-5' : ''
+            } 
+            ${
+              index === 2
+                ? 'sm:col-span-4 sm:row-span-2 sm:col-start-9 sm:row-start-3 col-span-2 col-start-3 row-start-5'
+                : ''
+            } 
+                ${index > 2 ? 'sm:col-span-2 sm:row-start-5 sm:col-start-auto col-span-2' : ''} 
+                 ${index > 5 ? 'hidden sm:block sm:col-span-2 sm:row-start-5' : ''} 
+          `}>
+            {index === 5 && images.length > 6 ? (
+              <PhotoView src={image}>
+                <div className="relative w-full h-full rounded-md cursor-pointer">
+                  <img
+                    src={image}
+                    alt={`Image ${index + 1}`}
+                    className="rounded-md w-full h-full object-cover object-center opacity-50 sm:opacity-100 group-hover:animate-flash"
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center rounded-md bg-black bg-opacity-50 sm:hidden">
+                    <span className="text-white text-lg font-bold">+{numberRestImages}</span>
+                  </div>
+                </div>
+              </PhotoView>
+            ) : (
+              <PhotoView src={image}>
+                <img
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  className="rounded-md w-full h-full object-cover object-center opacity-100 group-hover:opacity-100 group-hover:animate-flash cursor-pointer"
+                />
+              </PhotoView>
+            )}
+          </div>
+        ))}
+      </div>
+    </PhotoProvider>
+  )
+}
 
 export default Gallery
