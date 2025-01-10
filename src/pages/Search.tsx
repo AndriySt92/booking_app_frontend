0b94e8085c0d5d049c6b908013fba466bc05f57d
AndriySt92 <andriyua-1992@mail.ru>
2025-01-10 19:27:31 +0200

    feat: reset page on sort option and filter change, add scroll on page change. Add error handling

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState, useRef } from 'react'
 import { useSearchContext } from '../contexts/SearchContext'
 import {
   Pagination,
@@ -9,6 +9,7 @@ import {
   HotelCard,
   Select,
   NotFoundData,
+  Error,
 } from '../components'
 import { initialFilterValue, sortOptions } from '../config/hotelConfigs'
 import { useGetSearchHotels, useModalManager } from '../hooks'
@@ -20,15 +21,16 @@ const Search = () => {
   const [filter, setFilter] = useState<IFilterHotels>(initialFilterValue)
 
   const { closeModal, currentModal, openModal } = useModalManager()
-
   const { destination, checkIn, checkOut, childCount, adultCount } = useSearchContext()
-
   const { register, watch, reset } = useForm<{ sortOption: string }>({
     defaultValues: {
       sortOption: '',
     },
   })
   const sortOption = watch('sortOption')
+
+  // Ref for scrolling
+  const topDivRef = useRef<HTMLDivElement>(null)
 
   const searchParams = useMemo(
     () => ({
@@ -44,7 +46,13 @@ const Search = () => {
     [filter, sortOption, destination, checkIn, checkOut, childCount, adultCount, page],
   )
 
-  const { data: hotelData, isLoading, refetch } = useGetSearchHotels(searchParams)
+  const {
+    data: hotelData,
+    isLoading,
+    isError,
+    isSuccess,
+    refetch,
+  } = useGetSearchHotels(searchParams)
 
   const handleFilterApply = useCallback(
     ({ stars, types, facilities, maxPrice }: IFilterHotels) => {
@@ -64,74 +72,116 @@ const Search = () => {
     refetch()
   }, [refetch])
 
+  const handlePageChange = useCallback((newPage: number) => {
+    setPage(newPage)
+
+    // Scroll to the referenced div
+    if (topDivRef.current) {
+      topDivRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      })
+    }
+  }, [])
+
   useEffect(() => {
-    reset({ sortOption: '' })
+    if (sortOption) {
+      reset({ sortOption: '' })
+    }
   }, [destination, checkIn, checkOut, childCount, adultCount, filter])
 
-  const isHotelAvailble = hotelData?.data && hotelData.data.length > 0
+  useEffect(() => {
+    handlePageChange(1)
+  }, [sortOption, filter, handlePageChange])
+
+  const areHotelsAvailable = useMemo(
+    () => hotelData?.data && hotelData.data.length > 0,
+    [hotelData],
+  )
 
   return (
-    <div className="mb-7">
-      <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-        <div className="custom-shadow-rounded p-5 h-fit top-10 hidden lg:block">
-          <Filter
-            handleFilterApply={handleFilterApply}
-            handleClearFilters={handleClearFilters}
-            isLoading={isLoading}
-          />
-        </div>
-        {currentModal === 'filterModal' && (
-          <Modal onClose={closeModal}>
+    <>
+      {/* Main Content */}
+      <div className="mb-7" ref={topDivRef}>
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+          <div className="custom-shadow-rounded p-5 h-fit top-10 hidden lg:block">
             <Filter
               handleFilterApply={handleFilterApply}
               handleClearFilters={handleClearFilters}
               isLoading={isLoading}
             />
-          </Modal>
-        )}
-        <div className="flex flex-col gap-5">
-          <div className="flex flex-col lg:flex-row lg:items-center justify-between">
-            <div className="text-xl font-bold mb-5 lg:mb-0">
-              {hotelData?.pagination.total} Hotels found
-              {destination ? ` in ${destination}` : ''}
-            </div>
-            <div className="flex justify-between w-full lg:w-auto">
-              <div className="lg:hidden flex justify-end">
-                <Button
-                  classes="bg-blue-600 text-white hover:bg-blue-500"
-                  onClick={() => openModal('filterModal')}>
-                  Filter
-                </Button>
-              </div>
-
-              <Select
-                name="sortOption"
-                options={sortOptions}
-                placeholder="Sort by"
-                register={register}
-                disabled={!isHotelAvailble}
-              />
-            </div>
           </div>
-          {isLoading && <Loader />}
-          {isHotelAvailble ? (
-            hotelData.data.map((hotel) => (
-              <HotelCard role="searchCard" hotel={hotel} key={hotel._id} />
-            ))
-          ) : (
-            <NotFoundData title="No hotels match your criteria" />
-          )}
 
-          {isHotelAvailble && (
-            <Pagination
-              page={hotelData?.pagination.page || 1}
-              pages={hotelData?.pagination.pages || 1}
-              onPageChange={(page) => setPage(page)}
-            />
-          )}
+          <div className="flex flex-col gap-5 mt-1">
+            <div className="flex flex-col lg:flex-row lg:items-center justify-between">
+              {!isError && isSuccess && (
+                <div className="text-xl font-bold mb-5 lg:mb-0">
+                  {hotelData?.pagination.total} Hotels found
+                  {destination ? ` in ${destination}` : ''}
+                </div>
+              )}
+              <div className="flex justify-between w-full lg:w-auto">
+                <div className="lg:hidden flex justify-end">
+                  <Button
+                    classes="bg-blue-600 text-white hover:bg-blue-500"
+                    onClick={() => openModal('filterModal')}>
+                    Filter
+                  </Button>
+                </div>
+
+                <Select
+                  name="sortOption"
+                  options={sortOptions}
+                  placeholder="Sort by"
+                  register={register}
+                  disabled={!areHotelsAvailable}
+                />
+              </div>
+            </div>
+            {/* Loading State */}
+            {isLoading && <Loader />}
+
+            {/* Error State */}
+            {isError && (
+              <div className="text-center">
+                <Error message="Unable to fetch hotels." size="large" />
+              </div>
+            )}
+
+            {/* No Hotels Found */}
+            {isSuccess && !areHotelsAvailable && (
+              <NotFoundData title="No hotels match your criteria" />
+            )}
+
+            {/* Hotels List */}
+            {areHotelsAvailable &&
+              hotelData?.data.map((hotel) => (
+                <HotelCard role="searchCard" hotel={hotel} key={hotel._id} />
+              ))}
+
+            {/* Pagination */}
+            {areHotelsAvailable && (
+              <Pagination
+                page={page}
+                pages={hotelData?.pagination.pages || 1}
+                onPageChange={handlePageChange}
+              />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* Filter Modal for tablets and mobiles */}
+      {currentModal === 'filterModal' && (
+        <Modal onClose={closeModal}>
+          <Filter
+            handleFilterApply={handleFilterApply}
+            handleClearFilters={handleClearFilters}
+            isLoading={isLoading}
+          />
+        </Modal>
+      )}
+    </>
   )
 }
 
