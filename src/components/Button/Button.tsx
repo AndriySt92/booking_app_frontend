d748bafc0786120313e2f38341e4e5dd3835c5b9
AndriySt92 <andriyua-1992@mail.ru>
2025-01-16 18:10:20 +0200

    refactor: refine Props validation, add memo for Button, and rename prop classes to className

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,44 +1,56 @@
 import React from 'react'
+import cn from 'classnames'
 import { Link } from 'react-router-dom'
 
-interface Props {
+type ButtonProps = {
   children: React.ReactNode
-  classes: string
+  className?: string
   btnType?: 'submit' | 'button'
   disabled?: boolean
   onClick?: () => void
-  role?: 'button' | 'link'
-  to?: string
 }
 
-const Button = ({
-  children,
-  btnType = 'button',
-  classes,
-  disabled = false,
-  onClick,
-  role = 'button',
-  to = '',
-}: Props) => {
-  return (
-    <>
-      {role === 'button' ? (
-        <button
-          onClick={onClick}
-          type={btnType}
-          disabled={disabled}
-          className={`${classes} flex items-center justify-center rounded-lg py-1 px-3 sm:py-2 sm:px-4 font-semibold sm:text-lg cursor-pointer`}>
-          {children}
-        </button>
-      ) : (
-        <Link
-          to={to}
-          className={`${classes} flex items-center justify-center rounded-lg py-1 px-3 sm:py-2 sm:px-4 font-semibold sm:text-lg cursor-pointer`}>
-          {children}
-        </Link>
-      )}
-    </>
-  )
+type LinkProps = {
+  role: 'link'
+  to: string // Required when role is "link"
 }
+
+type ButtonOnlyProps = {
+  role?: 'button' // Default to "button"
+  to?: never // Prevent `to` when role is "button"
+}
+
+type Props = ButtonProps & (LinkProps | ButtonOnlyProps)
+
+const Button = React.memo(
+  ({
+    children,
+    btnType = 'button',
+    className,
+    disabled = false,
+    onClick,
+    role = 'button',
+    to = '',
+  }: Props) => {
+    const classNames = cn(
+      'flex items-center justify-center rounded-lg py-1 px-3 sm:py-2 sm:px-4 font-semibold sm:text-lg cursor-pointer',
+      className,
+    )
+
+    return (
+      <>
+        {role === 'button' ? (
+          <button onClick={onClick} type={btnType} disabled={disabled} className={classNames}>
+            {children}
+          </button>
+        ) : (
+          <Link to={to} className={classNames}>
+            {children}
+          </Link>
+        )}
+      </>
+    )
+  },
+)
 
 export default Button
