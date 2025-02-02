18edddd8bb636b06c3bd2a9cd4683eb0f342f483
AndriySt92 <andriyua-1992@mail.ru>
2025-02-02 21:56:32 +0200

    feat: add prop center to centralize text in Error.tsx

--- src/components/Common/Error.tsx
+++ src/components/Common/Error.tsx
@@ -1,13 +1,15 @@
 interface Props {
   message: string
   size?: 'small' | 'large'
+  center?: boolean
 }
 
-const Error = ({ message, size }: Props) => {
+const Error = ({ message, size = 'large', center }: Props) => {
   const baseClasses = 'text-red-600 font-semibold'
   const sizeClasses = size === 'small' ? 'text-sm' : 'text-2xl'
+  const textAlignClass = center ? 'text-center' : ''
 
-  return <div className={`${baseClasses} ${sizeClasses}`}>{message}</div>
+  return <div className={`${baseClasses} ${sizeClasses} ${textAlignClass}`}>{message}</div>
 }
 
 export default Error
