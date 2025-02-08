3fe6cc25531f2f9324d5ba0ce588c9f6ba03852d
AndriySt92 <andriyua-1992@mail.ru>
2025-02-08 22:54:38 +0200

    feat: add description for NotFoundData component

--- src/components/Common/NotFoundData.tsx
+++ src/components/Common/NotFoundData.tsx
@@ -2,17 +2,19 @@ import notFoundImageUrl from '../../assets/notFound.svg'
 
 interface Props {
   title: string
+  description?: string
 }
 
-const NotFoundData = ({ title }: Props) => {
+const NotFoundData = ({ title, description }: Props) => {
   return (
-    <div className="mx-auto">
+    <div className="flex flex-col justify-center gap-2">
       <img
         src={notFoundImageUrl}
         alt="No hotels found"
         className="mx-auto w-full h-full max-w-[500px] max-h-[500px]"
       />
-      <div className="text-semibold text-xl text-center">{title}</div>
+      <div className="text-bold text-2xl text-center">{title}</div>
+      <div className="text-semibold text-xl text-center">{description}</div>
     </div>
   )
 }
