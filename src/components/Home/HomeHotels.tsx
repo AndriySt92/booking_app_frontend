1a89b00173bf9bc7146cb9e0b1ce3b73dca393c6
AndriySt92 <andriyua-1992@mail.ru>
2025-02-11 20:53:59 +0200

    fix: change tag in HomeHotels and HomeSlider components

--- src/components/Home/HomeHotels.tsx
+++ src/components/Home/HomeHotels.tsx
@@ -9,13 +9,11 @@ const HomeHotels = ({ hotels }: Props) => {
   return (
     <div>
       <h2 className="text-3xl font-bold">Latest Destinations</h2>
-      <p className="sm:text-lg mb-5">Most recent desinations added by our hosts</p>
-      <div className="grid">
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
-          {hotels.map((hotel) => (
-            <HomeHotelCard hotel={hotel} key={hotel._id} />
-          ))}
-        </div>
+      <div className="sm:text-lg mb-5">Most recent desinations added by our hosts</div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+        {hotels.map((hotel) => (
+          <HomeHotelCard hotel={hotel} key={hotel._id} />
+        ))}
       </div>
     </div>
   )
--- src/components/Home/HomeSlider.tsx
+++ src/components/Home/HomeSlider.tsx
@@ -11,9 +11,9 @@ const HomeSlider = ({ hotelsCountriesSummary }: Props) => {
   return (
     <div>
       <h2 className="text-3xl font-bold">Explore Popular Destinations</h2>
-      <p className="sm:text-lg mb-5">
+      <div className="sm:text-lg mb-5">
         Discover the most popular countries and their unique stays, handpicked for you.
-      </p>
+      </div>
       <div className="-mx-[6px]">
         <Slider {...sliderSettings}>
           {hotelsCountriesSummary.map((item) => (
