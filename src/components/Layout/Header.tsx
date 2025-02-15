de8ebb6401d4d7fc3d11644e48feb274c96bca37
AndriySt92 <andriyua-1992@mail.ru>
2025-02-15 16:37:02 +0200

    feat: add scroll on route change

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useAppContext } from '../../contexts/AppContext'
 import { Button, SignOutButton } from '../'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
-const Header = () => {
+interface HeaderProps {
+  scrollToContent: React.RefObject<HTMLDivElement>
+}
+
+const Header = ({ scrollToContent }: HeaderProps) => {
   const { isLoggedIn } = useAppContext()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const location = useLocation()
+
+  // Scroll when route changes and close menu
+  useEffect(() => {
+    if (location.pathname === '/my-bookings' || location.pathname === '/my-hotels') {
+      setTimeout(() => {
+        scrollToContent?.current?.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        })
+      }, 100)
+
+      setIsMenuOpen(false)
+    }
+
+    window.scrollTo({ top: 0 })
+  }, [location.pathname])
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
@@ -14,57 +35,63 @@ const Header = () => {
 
   return (
     <div className="bg-blue-800 py-6 border-b-2 border-white md:border-b-0">
-    <div className="container mx-auto flex justify-between items-center">
-      <span className="text-3xl text-white font-bold tracking-tight">
-        <Link to="/">Booking.com</Link>
-      </span>
-      <div className={`${isLoggedIn ? 'hidden' : 'flex'} md:flex justify-center items-center space-x-2`}>
-        {isLoggedIn ? (
-          <>
-            <Button role="link" to="/my-bookings" className="text-white hover:bg-blue-600">
-              My Bookings
-            </Button>
-            <Button role="link" to="/my-hotels" className="text-white hover:bg-blue-600">
-              My Hotels
-            </Button>
-            <SignOutButton />
-          </>
-        ) : (
-          <Button role="link" to="/sign-in" className="text-blue-600 bg-white hover:bg-gray-100">
-            Sign In
-          </Button>
-        )}
-      </div>
-      <div className={`${isLoggedIn ? 'md:hidden' : 'hidden'} flex items-center`}>
-        <Button onClick={toggleMenu} className="text-white focus:outline-none">
-          {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-        </Button>
-      </div>
-    </div>
-    <div className={`${isLoggedIn ? 'md:hidden' : 'hidden'} overflow-hidden transition-max-height duration-100 ease-in-out`}>
-      <div className={`menu-content ${isMenuOpen ? 'open' : ''}`}>
-        <div className="flex flex-col items-center justify-center space-y-2 py-2">
-          {isLoggedIn && (
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">Booking.com</Link>
+        </span>
+        <div
+          className={`${
+            isLoggedIn ? 'hidden' : 'flex'
+          } md:flex justify-center items-center space-x-2`}>
+          {isLoggedIn ? (
             <>
-              <Button
-                role="link"
-                to="/my-bookings"
-                className="text-white hover:bg-blue-600 text-center">
+              <Button role="link" to="/my-bookings" className="text-white hover:bg-blue-600">
                 My Bookings
               </Button>
-              <Button
-                role="link"
-                to="/my-hotels"
-                className="text-white hover:bg-blue-600 text-center">
+              <Button role="link" to="/my-hotels" className="text-white hover:bg-blue-600">
                 My Hotels
               </Button>
               <SignOutButton />
             </>
+          ) : (
+            <Button role="link" to="/sign-in" className="text-blue-600 bg-white hover:bg-gray-100">
+              Sign In
+            </Button>
           )}
+        </div>
+        <div className={`${isLoggedIn ? 'md:hidden' : 'hidden'} flex items-center`}>
+          <Button onClick={toggleMenu} className="text-white focus:outline-none">
+            {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+          </Button>
+        </div>
+      </div>
+      <div
+        className={`${
+          isLoggedIn ? 'md:hidden' : 'hidden'
+        } overflow-hidden transition-max-height duration-100 ease-in-out`}>
+        <div className={`menu-content ${isMenuOpen ? 'open' : ''}`}>
+          <div className="flex flex-col items-center justify-center space-y-2 py-2">
+            {isLoggedIn && (
+              <>
+                <Button
+                  role="link"
+                  to="/my-bookings"
+                  className="text-white hover:bg-blue-600 text-center">
+                  My Bookings
+                </Button>
+                <Button
+                  role="link"
+                  to="/my-hotels"
+                  className="text-white hover:bg-blue-600 text-center">
+                  My Hotels
+                </Button>
+                <SignOutButton />
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
 }
 
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import { Footer, Header, Hero, SearchBar } from '../'
 
 interface Props {
@@ -5,15 +6,19 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
+  const contentRef = useRef<HTMLDivElement>(null)
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Header scrollToContent={contentRef} />
       <Hero />
       <div className="container mx-auto">
         <SearchBar />
       </div>
 
-      <div className="container mx-auto py-10 sm:py-14 flex-1 h-full">
+      <div
+        ref={contentRef}
+        className="container mx-auto py-10 sm:py-14 flex-1 h-full scroll-mt-[120px] md:scroll-mt-[-30px]">
         <div className="w-full">{children}</div>
       </div>
       <Footer />
