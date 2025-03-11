906a4091cfa748a055eda0109a9dcbc09b9322ea
AndriySt92 <andriyua-1992@mail.ru>
2025-03-11 21:27:41 +0200

    style: add animation for StarRating. Add cotainer div

--- src/components/Common/StarRating.tsx
+++ src/components/Common/StarRating.tsx
@@ -1,16 +1,23 @@
 import { AiFillStar } from 'react-icons/ai'
+import cn from 'classnames'
 
 interface Props {
   starRating: number
+  className?: string
 }
 
-const StarRating = ({ starRating }: Props) => {
+const StarRating = ({ starRating, className }: Props) => {
   return (
-    <>
+    <div className={cn(className, 'flex items-center space-x-1')}>
       {Array.from({ length: starRating }).map((_, i) => (
-        <AiFillStar className="fill-yellow-400" key={i} />
+        <AiFillStar
+          key={i}
+          className="star-animation transition-transform duration-300 text-yellow-500"
+          style={{ animationDelay: `${i * 0.3}s` }}
+          size={20}
+        />
       ))}
-    </>
+    </div>
   )
 }
 
