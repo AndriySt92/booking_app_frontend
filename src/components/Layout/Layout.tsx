d7b66ec33b7f04f042536cf95ea08c98400f425a
AndriySt92 <andriyua-1992@mail.ru>
2025-03-11 21:23:52 +0200

    style: update scroll-mt in Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,18 +7,18 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   const contentRef = useRef<HTMLDivElement>(null)
-
+  console.log('Layout')
   return (
     <div className="flex flex-col min-h-screen">
       <Header scrollToContent={contentRef} />
       <Hero />
-      <div className="container mx-auto">
+      <div className="container mx-auto relative z-20">
         <SearchBar />
       </div>
 
       <div
         ref={contentRef}
-        className="container mx-auto py-10 sm:py-14 flex-1 h-full scroll-mt-[170px] md:scroll-mt-[-30px]">
+        className="container mx-auto py-10 sm:py-14 flex-1 h-full scroll-mt-[240px] md:scroll-mt-[50px]">
         <div className="w-full">{children}</div>
       </div>
       <Footer />
