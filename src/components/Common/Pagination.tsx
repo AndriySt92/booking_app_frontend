e1abf3dc1a363692d57d6886b3ac76f7ce8b4436
AndriySt92 <andriyua-1992@mail.ru>
2025-03-14 17:22:09 +0200

    style: adjust styles of pagination component

--- src/components/Common/Pagination.tsx
+++ src/components/Common/Pagination.tsx
@@ -1,5 +1,7 @@
 import cn from 'classnames'
 import { generatePagination } from '../../utils/generatePagination'
+import Text from './Text'
+import Button from '../Buttons/Button'
 
 interface Props {
   page: number
@@ -11,22 +13,33 @@ const Pagination = ({ page, pages, onPageChange }: Props) => {
   const paginationItems = generatePagination(page, pages)
 
   return (
-    <div className="flex justify-center">
-      <ul className="flex">
+    <div className="flex justify-center py-3">
+      <ul className="flex gap-1 sm:gap-2 items-center">
         {paginationItems.map((item, index) => (
-          <li className="flex items-end" key={index}>
+          <li className="flex items-end h-full" key={index}>
             {typeof item === 'number' ? (
-              <button
-                className={cn('px-2 py-1 sm:px-3 sm:py-2 mx-1 font-semibold rounded ', {
-                  'border border-slate-300': typeof item === 'number',
-                  'bg-slate-300 cursor-default font-bold': page === item,
-                  'hover:bg-slate-200 hover:border-slate-400': page !== item,
-                })}
-                onClick={() => onPageChange(item)}>
+              <Button
+                className={cn(
+                  'min-w-[35px] h-[40px] sm:min-w-[40px] sm:h-[45px] !px-2 !sm:px-3',
+                  'rounded-lg border duration-300',
+                  'hover:shadow-md',
+                  {
+                    'bg-blue-600 border-blue-700 text-white shadow-lg': page === item,
+                    'border-gray-300 text-gray-700 hover:bg-gray-50': page !== item,
+                  },
+                )}
+                onClick={() => onPageChange(item)}
+                aria-current={page === item ? 'page' : undefined}>
                 {item}
-              </button>
+              </Button>
             ) : (
-              <span className="mx-1 tracking-widest">{item}</span>
+              <Text
+                className="px-2 sm:px-3 select-none tracking-widest"
+                as="p"
+                color="gray-800"
+                weight="medium">
+                {item}
+              </Text>
             )}
           </li>
         ))}
