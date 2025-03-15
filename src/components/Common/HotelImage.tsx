265403357ab7345479077e9d42581094a6e0542f
AndriySt92 <andriyua-1992@mail.ru>
2025-03-15 19:42:25 +0200

    feat: implement sign-in redirect on favorite click when !isLoggedIn in HotelImage.tsx

--- src/components/Common/HotelImage.tsx
+++ src/components/Common/HotelImage.tsx
@@ -1,7 +1,8 @@
 import { FaRegHeart, FaHeart } from 'react-icons/fa'
 import { ImSpinner8 } from 'react-icons/im'
-import { useAddFavorite, useRemoveFavorite } from '../../hooks'
+import { useAddFavorite, useAppContext, useRemoveFavorite } from '../../hooks'
 import cn from 'classnames'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 interface Props {
   imageUrl: string
@@ -13,9 +14,18 @@ interface Props {
 const HotelImage = ({ imageUrl, isFavorite, hotelId, className }: Props) => {
   const { mutate: addFavorite, isLoading: isAdding } = useAddFavorite(hotelId)
   const { mutate: removeFavorite, isLoading: isRemoving } = useRemoveFavorite(hotelId)
+  const { isLoggedIn } = useAppContext()
+  const navigate = useNavigate()
+  const location = useLocation()
 
-  const handleFavoriteClick = (e: React.MouseEvent) => {
+  const handleFavoriteAction = (e: React.MouseEvent) => {
     e.stopPropagation()
+
+    if (!isLoggedIn) {
+      navigate('/sign-in', { state: { from: location } })
+      return
+    }
+
     isFavorite ? removeFavorite() : addFavorite()
   }
 
@@ -32,11 +42,13 @@ const HotelImage = ({ imageUrl, isFavorite, hotelId, className }: Props) => {
         className="w-full h-full object-cover object-center transform group-hover:scale-105 transition-transform duration-300"
         alt="Hotel room"
       />
-      <div className="absolute top-3 right-3 text-2xl cursor-pointer" onClick={handleFavoriteClick}>
+      <div
+        className="absolute top-3 right-3 text-2xl cursor-pointer"
+        onClick={handleFavoriteAction}>
         <div className="flex justify-center items-center w-[35px] h-[35px] rounded-full bg-white shadow-lg hover:shadow-xl transition-shadow">
           {isLoading ? (
             <ImSpinner8 className="text-blue-600 animate-spin" size={20} />
-          ) : isFavorite ? (
+          ) : isLoggedIn && isFavorite ? (
             <FaHeart
               className="text-red-500 transition-transform duration-300 hover:scale-110"
               size={20}
