1a3ed6d447b9ee11079b2fbbf30b7e21c7b88d3c
AndriySt92 <andriyua-1992@mail.ru>
2025-03-15 19:37:37 +0200

    refactor: optimize fetching of favorite hotel IDs

--- src/hooks/useGetFavoritesIds.ts
+++ src/hooks/useGetFavoritesIds.ts
@@ -3,13 +3,14 @@ import { fetchFavoriteHotelIds } from '../services/favoritesApi'
 import { useAppContext } from '.'
 
 const useGetFavoriteIds = () => {
-  const { showToast } = useAppContext()
+  const { showToast, isLoggedIn } = useAppContext()
 
   return useQuery('fetchFavoriteHotelIds', fetchFavoriteHotelIds, {
     onError: (error: Error) => {
       const message = error.message || 'Error fetching favorite hotel ids'
       showToast({ message, type: 'ERROR' })
     },
+    enabled: isLoggedIn,
     staleTime: Infinity,
   })
 }
